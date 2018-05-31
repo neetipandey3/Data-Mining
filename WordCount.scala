715c958e62bd02de2c3f7f992039bab66e750cc7
Neeti <neetipandey3@gmail.com>
2018-05-30 20:22:53 -0700

    word count

---
+++ WordCount.scala
@@ -0,0 +1,45 @@
+/**
+  * Word Count
+  *
+  * Usage: spark-submit --class WordCount
+  */
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object WordCount {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+    conf.setAppName("MovieLensAvgRating")
+    conf.setMaster("local[*]")
+    conf.set("spark.driver.allowMultipleContexts", "true")
+
+    val sc = new SparkContext(conf)
+
+
+    // Load the data
+    val readData = sc.textFile(args(0))
+
+    // Generate list of words
+    val wordsList = readData.flatMap(line =>
+    line.split("\\W+"))
+      .map(each => each.toLowerCase)
+
+    // Create tuples (Word, 1)
+    val tuples = wordsList.map(word =>
+      (word, 1))
+
+    val wordsCount = tuples.reduceByKey((w1, w2) => (w1+w2)).sortByKey(true)
+
+    wordsCount.map{case(word, count) =>
+      var line = word.toString + "," + count.toString
+      line
+    }.coalesce(1).saveAsTextFile(args(1))
+
+
+    sc.stop()
+
+
+  }
+
+}
